73ad7029c1ee2815cb8b66a5d061aca4dcd8cddb
raavanan30 <51847005+raavanan30@users.noreply.github.com>
2019-12-18 09:07:27 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var options = {
+  animateCanvas: false,
+  phaseThreshold: false,
+  preloader: true,
+  reverseWhenNavigatingBackwards: true
+}
+
+// Launch Sequence on the element, and with the options we specified above
+var mySequence = sequence(sequenceElement, options);
